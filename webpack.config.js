2f3e4b4d6263da1abfce38046e6473c223f53cee
Anton Volkov <volkovanton71@yandex.ru>
2020-04-17 19:37:53 +0300

    correct webpack.config.js file-loader:name

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
                 {
                     loader: 'file-loader',
                     options: {
-                        name: '[path][name].[ext]',
+                        name: '[name].[ext]',
                         outputPath: 'images/'
                     }
                 }
